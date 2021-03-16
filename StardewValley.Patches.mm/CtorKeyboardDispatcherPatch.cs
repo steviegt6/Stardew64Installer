257308c030ac56518a81da8f9e8fee93133b9a19
Stevie <xxlennygamerxx@gmail.com>
2021-03-16 00:37:59 -0700

    Change project types and organize projects

---
+++ StardewValley.Patches.mm/CtorKeyboardDispatcherPatch.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Xna.Framework;
+using MonoMod;
+
+namespace StardewValley.Patches.mm
+{
+    // ReSharper disable once ArrangeTypeModifiers
+    [MonoModPatch("global::StardewValley.KeyboardDispatcher.ctor")]
+    class CtorKeyboardDispatcherPatch : KeyboardDispatcher
+    {
+        [KeyboardDispatcherCtor]
+        [SuppressMessage("Style", "IDE1006:Naming Styles")]
+        public extern void orig_ctor();
+
+        public CtorKeyboardDispatcherPatch(GameWindow window) : base(window) => orig_ctor();
+    }
+}
