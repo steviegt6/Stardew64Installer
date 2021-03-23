3eac7064f4b21eb457722dceb2f00aa8a154d3b7
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2021-03-22 23:52:52 -0400

    Replace manual sleeps with checked operations
    With this commit:
    * The installer now waits automatically for the operation to finish when copying/deleting files or running processes. That simplifies the main code and makes the installer much faster, since we don't need to sleep manually.
    * Processes now run in the same window, so it's easier to troubleshoot if a process fails.

---
+++ SDV.Installer/Framework/FileUtilities.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Threading;
+
+namespace SDV.Installer.Framework
+{
+    /// <summary>Provides utility methods for file operations.</summary>
+    internal static class FileUtilities
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>Copy a file and wait until the copy operation completes.</summary>
+        /// <param name="file">The file to copy.</param>
+        /// <param name="toPath">The absolute destination file path.</param>
+        public static void CopyToAndWait(this FileInfo file, string toPath)
+        {
+            file.CopyTo(toPath, overwrite: true);
+            while (!File.Exists(toPath))
+                Thread.Sleep(100);
+        }
+
+        /// <summary>Delete a file and wait until the deletion completes.</summary>
+        /// <param name="file">The file to copy.</param>
+        /// <returns>Returns whether the file existed and was deleted.</returns>
+        public static bool DeleteAndWait(this FileInfo file)
+        {
+            if (!file.Exists)
+                return false;
+
+            file.Delete();
+            while (File.Exists(file.FullName))
+                Thread.Sleep(100);
+            return true;
+        }
+    }
+}
